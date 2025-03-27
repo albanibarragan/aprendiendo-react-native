a3971a1502c0d248142d87077792029d149323db
albani <155846628+albanibarragan@users.noreply.github.com>
2025-03-27 19:20:30 -0400

    practicas

--- curso-react-native-metacritic-app/App.js
+++ curso-react-native-metacritic-app/App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { getLatestGames } from "../lib/metacritic";
 
 const flow = require('./assets/flow-poster.jpg')
 
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then(games => setGames(games));
+  }, []);
   return (
     <View style={styles.container}>
-      <Image onLoad={() => console.log('Image loaded')}  source={flow} style={{width: 300, height: 300, resizeMode: 'contain',  borderRadius: 16} } />
-      <Text>Estamos trabajando para ti ✈ 😊</Text>
-      <StatusBar style="auto" />
+     <StatusBar style="auto" />
+     {games.map(games => (
+      <View key={games.slug} style={styles.card}>
+        <Image source={{uri: games.poster}} style={styles.poster} />
+        <Text>{games.title}</Text>
+        <Text>{games.year}</Text>
+        <Text>{games.score}</Text>
+      </View> 
+     ))}
     </View>
   );
 }
@@ -21,4 +34,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#f0f0f0',
   },
+  poster: {
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+  },
+  card:{
+
+  }
 });
